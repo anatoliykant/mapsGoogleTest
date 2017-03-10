06dfea3e5ce2bed520bb1ca6f4f199ce903a9e68
AnatoliyKant <Kant@Anatolijs-iMac.local>
2017-03-10 06:58:13 +0300

    Completed test project

--- mapsGoogleTest/ViewController.swift
+++ mapsGoogleTest/ViewController.swift
@@ -9,14 +9,33 @@
 import UIKit
 import GoogleMaps
 
-class ViewController: UIViewController {
+class VacationDestination: NSObject {
+    
+    let name: String
+    let location: CLLocationCoordinate2D
+    let zoom: Float
+    
+    init(name: String, location: CLLocationCoordinate2D, zoom: Float) {
+        self.name = name
+        self.location = location
+        self.zoom = zoom
+    }
+}
 
+class ViewController: UIViewController {
+    
+    var mapView: GMSMapView?
+    
+    var currentDestination: VacationDestination?
+    
+    let destinations = [VacationDestination(name: "Embarcadero Station", location: CLLocationCoordinate2DMake(37.793117, -122.396995), zoom: 15.0), VacationDestination(name: "Ferry Building", location: CLLocationCoordinate2DMake(37.795362, -122.393422), zoom: 18.0)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         GMSServices.provideAPIKey("AIzaSyC3p4XP7YA6td5wpxXmZlCo137Pe1z--Wc")
         let camera = GMSCameraPosition.camera(withLatitude: 37.621723, longitude: -122.380542, zoom: 12.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
         
@@ -25,8 +44,35 @@ class ViewController: UIViewController {
         marker.title = "SFO Airport"
         marker.map = mapView
         
+        //navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: "next")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextMarker))
+        
     }
-
-
+    
+    
+    func nextMarker() {
+        
+        if currentDestination == nil || currentDestination == destinations.last {
+            currentDestination = destinations.first
+        } else {
+            if let index = destinations.index(of: currentDestination!) {
+                currentDestination = destinations[index + 1]
+            }
+        }
+        setMapCamera()
+    }
+    
+    private func setMapCamera() {
+        CATransaction.begin()
+        CATransaction.setValue(2, forKey: kCATransactionAnimationDuration)
+        mapView?.animate(to: GMSCameraPosition.camera(withTarget: currentDestination!.location, zoom: currentDestination!.zoom))
+        CATransaction.commit()
+        
+        let marker  = GMSMarker(position: currentDestination!.location)
+        marker.title = currentDestination!.name
+        marker.map = mapView
+    }
+    
+    
 }
 
